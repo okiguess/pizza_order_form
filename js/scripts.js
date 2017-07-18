853da9ef66dcebf410e6693efbd9b48eecd95ccf
Camden Swita <camdenswita@gmail.com>
2017-07-18 16:44:56 -0700

    Add basic HTML structure, pizza object constructor and pizza object prototypes

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Pizza(toppings, size, price, userName) {
+  this.toppings = meat;
+  this.size = size;
+  this.price = price;
+  this.userName = userName;
+}
+
+Pizza.prototype.userName = function() {
+  this.userName = $("input#submitName").val();
+}
+
+Pizza.prototype.pizzaOrder = function() {
+  this.size =
+  this.toppings =
+
+}
